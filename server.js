9d4a6458006729f7e7d104fa946bf319b2e24ca7
Tupark000 <tupark000@gmail.com>
2025-04-25 01:36:55 +0800

    Initial commit for web app backend

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const db = require('./db');
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static('public')); // Serves public HTML files
+
+// Test route
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
