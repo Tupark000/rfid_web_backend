63f0dfbdf60ed105039dff289e232eedc0a35585
Tupark000 <tupark000@gmail.com>
2025-04-25 03:43:31 +0800

    Change: Login

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static('public')); // Serves public HTML files
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Main Admin Login Route
 app.post('/login', (req, res) => {
