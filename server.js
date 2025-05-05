e7ff426896c8cb5f1d3ec4a862151263216f5c59
Tupark000 <tupark000@gmail.com>
2025-05-05 12:25:30 +0800

    Update: server.js

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.get('/', (req, res) => {
+  res.send('Backend is working!');
+});
+
 // Serve HTML files directly
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
